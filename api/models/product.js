2c157b340f50f28035dd41105f234eea3062b48b
Kushal KC <kushal198@gmail.com>
2019-07-30 19:46:25 +0545

    Mongoose validation

--- api/models/product.js
+++ api/models/product.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String },
-  price: { type: Number }
+  name: { type: String, required:true },
+  price: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Product', productSchema);
