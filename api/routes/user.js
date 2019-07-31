0befef25b7ee8f822c158cad9a2e39666d525a04
Kushal KC <kushal198@gmail.com>
2019-07-31 21:23:25 +0545

    completed project

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,60 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
+require('dotenv').config();
+const checkAuth = require('../middleware/check-auth');
 
-router.post('/signup', (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then(user => {
-      if (user.length >= 1) {
-        return res.status(409).json({
-          message: 'Email Already exists'
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            res.status(500).json({
-              error: err
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash
-            });
-            user
-              .save()
-              .then(result => {
-                console.log(result);
-                res.status(201).json({
-                  message: 'User created'
-                });
-              })
-              .catch(err => {
-                res.status(500).json({
-                  error: err
-                });
-              });
-          }
-        });
-      }
-    });
-});
-router.delete('/:userId', (req, res, next) => {
-  User.deleteOne({ _id: req.params.userId })
-    .exec()
-    .then(result => {
-      res.status(200).json({
-        message: 'User deleted'
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      });
-    });
-});
+const UserControllers = require('../controllers/user');
+
+router.post('/signup', UserControllers.user_signup);
+
+router.post('/login', UserControllers.user_login);
+
+router.delete('/:userId', checkAuth, UserControllers.user_delete);
 
 module.exports = router;
